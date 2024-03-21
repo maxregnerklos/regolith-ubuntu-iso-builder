8f613a2948fe14f08ef1d162f8e93368af2d39d4
maxregnerklos <135330858+maxregnerklos@users.noreply.github.com>
2024-03-21 08:56:31 +0100

    Update chroot_build.sh

--- scripts/chroot_build.sh
+++ scripts/chroot_build.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
+# Rephrased code for building Ubuntu from scratch
 
 set -e                  # exit on error
 set -o pipefail         # exit on pipeline error
 set -u                  # treat unset variable as error
 #set -x
 
-DIR="$(dirname "$(readlink -f "$0")")"
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-COMMANDS=(setup_host debootstrap run_chroot build_iso)
-
-DATE=`TZ="UTC" date +"%y%m%d-%H%M%S"`
+COMMANDS=(setup_host install_pkg finish_up)
 
 function help() {
     # if $1 is set, use $1 as headline message in help()
     if [ -z ${1+x} ]; then
-        echo -e "This script builds a bootable ubuntu-bsd ISO image"
+        echo -e "This script builds Ubuntu from scratch"
         echo -e
     else
         echo -e $1
@@ -44,247 +43,134 @@ function find_index() {
     help "Command not found : $1"
 }
 
-function enter_setup() {
-    sudo mount --bind /dev chroot/dev
-    sudo mount --bind /run chroot/run
-    sudo chroot chroot mount none -t proc /proc
-    sudo chroot chroot mount none -t sysfs /sys
-    sudo chroot chroot mount none -t devpts /dev/pts
-}
-
-function exit_teardown() {
-    sudo chroot chroot umount /proc
-    sudo chroot chroot umount /sys
-    sudo chroot chroot umount /dev/pts
-    sudo umount chroot/dev
-    sudo umount chroot/run
-}
-
 function check_host() {
-    local os_ver
-    os_ver=`lsb_release -i | grep -E "(Ubuntu|Debian)"`
-    if [[ -z "$os_ver" ]]; then
-        echo "WARNING : OS is not Debian or Ubuntu and is untested"
-    fi
-
-    if [ $(id -u) -eq 0 ]; then
-        echo "This script should not be run as 'root'"
+    if [ $(id -u) -ne 0 ]; then
+        echo "This script should be run as 'root'"
         exit 1
     fi
-}
 
-# Load configuration values from file
-function load_config() {
-    if [[ -f "$DIR/config.sh" ]]; then
-        . "$DIR/config.sh"
-    elif [[ -f "$DIR/default_config.sh" ]]; then
-        . "$DIR/default_config.sh"
-    else
-        >&2 echo "Unable to find default config file  $DIR/default_config.sh, aborting."
-        exit 1
-    fi
-}
-
-# Verify that necessary configuration values are set and they are valid
-function check_config() {
-    local expected_config_version
-    expected_config_version="0.4"
-
-    if [[ "$CONFIG_FILE_VERSION" != "$expected_config_version" ]]; then
-        >&2 echo "Invalid or old config version $CONFIG_FILE_VERSION, expected $expected_config_version. Please update your configuration file from the default."
-        exit 1
-    fi
+    export HOME=/root
+    export LC_ALL=C
 }
 
 function setup_host() {
     echo "=====> running setup_host ..."
-    sudo apt update
-    sudo apt install -y binutils debootstrap squashfs-tools xorriso grub-pc-bin grub-efi-amd64-bin mtools dosfstools unzip
-    sudo mkdir -p chroot
-}
 
-function debootstrap() {
-    echo "=====> running debootstrap ... will take a couple of minutes ..."
-    sudo debootstrap  --arch=amd64 --variant=minbase $TARGET_UBUNTU_VERSION chroot $TARGET_UBUNTU_MIRROR
-}
+   cat <<EOF > /etc/apt/sources.list
+deb $TARGET_UBUNTU_MIRROR $TARGET_UBUNTU_VERSION main restricted universe multiverse
+deb-src $TARGET_UBUNTU_MIRROR $TARGET_UBUNTU_VERSION main restricted universe multiverse
 
-function run_chroot() {
-    echo "=====> running run_chroot ..."
+deb $TARGET_UBUNTU_MIRROR $TARGET_UBUNTU_VERSION-security main restricted universe multiverse
+deb-src $TARGET_UBUNTU_MIRROR $TARGET_UBUNTU_VERSION-security main restricted universe multiverse
 
-    enter_setup
-
-    # Setup build scripts in chroot environment
-    sudo ln -f $DIR/chroot_build.sh chroot/root/chroot_build.sh
-    sudo ln -f $DIR/default_config.sh chroot/root/default_config.sh
-    if [[ -f "$DIR/config.sh" ]]; then
-        sudo ln -f $DIR/config.sh chroot/root/config.sh
-    fi
-
-    # Launch into chroot environment to build install image.
-    sudo chroot chroot /usr/bin/env DEBIAN_FRONTEND=${DEBIAN_FRONTEND:-readline} /root/chroot_build.sh -
-
-    # Cleanup after image changes
-    sudo rm -f chroot/root/chroot_build.sh
-    sudo rm -f chroot/root/default_config.sh
-    if [[ -f "chroot/root/config.sh" ]]; then
-        sudo rm -f chroot/root/config.sh
-    fi
-
-    exit_teardown
-}
-
-function build_iso() {
-    echo "=====> running build_iso ..."
-
-    rm -rf image
-    mkdir -p image/{casper,isolinux,install}
-
-    # copy kernel files
-    sudo cp chroot/boot/vmlinuz-**-**-generic image/casper/vmlinuz
-    sudo cp chroot/boot/initrd.img-**-**-generic image/casper/initrd
-
-    # memtest86
-    sudo cp chroot/boot/memtest86+.bin image/install/memtest86+
-
-    wget --progress=dot https://www.memtest86.com/downloads/memtest86-usb.zip -O image/install/memtest86-usb.zip
-    unzip -p image/install/memtest86-usb.zip memtest86-usb.img > image/install/memtest86
-    rm -f image/install/memtest86-usb.zip
-
-    # grub
-    touch image/ubuntu
-    cat <<EOF > image/isolinux/grub.cfg
-
-search --set=root --file /ubuntu
-
-insmod all_video
-
-set default="0"
-set timeout=30
-
-menuentry "${GRUB_LIVEBOOT_LABEL}" {
-   linux /casper/vmlinuz boot=casper nopersistent toram quiet splash ---
-   initrd /casper/initrd
-}
-
-menuentry "${GRUB_INSTALL_LABEL}" {
-   linux /casper/vmlinuz boot=casper only-ubiquity quiet splash ---
-   initrd /casper/initrd
-}
-
-menuentry "Check disc for defects" {
-   linux /casper/vmlinuz boot=casper integrity-check quiet splash ---
-   initrd /casper/initrd
-}
-
-menuentry "Test memory Memtest86+ (BIOS)" {
-   linux16 /install/memtest86+
-}
-
-menuentry "Test memory Memtest86 (UEFI, long load time)" {
-   insmod part_gpt
-   insmod search_fs_uuid
-   insmod chain
-   loopback loop /install/memtest86
-   chainloader (loop,gpt1)/efi/boot/BOOTX64.efi
-}
+deb $TARGET_UBUNTU_MIRROR $TARGET_UBUNTU_VERSION-updates main restricted universe multiverse
+deb-src $TARGET_UBUNTU_MIRROR $TARGET_UBUNTU_VERSION-updates main restricted universe multiverse
 EOF
 
-    # generate manifest
-    sudo chroot chroot dpkg-query -W --showformat='${Package} ${Version}\n' | sudo tee image/casper/filesystem.manifest
-    sudo cp -v image/casper/filesystem.manifest image/casper/filesystem.manifest-desktop
-    for pkg in $TARGET_PACKAGE_REMOVE; do
-        sudo sed -i "/$pkg/d" image/casper/filesystem.manifest-desktop
-    done
+    echo "$TARGET_NAME" > /etc/hostname
 
-    # compress rootfs
-    sudo mksquashfs chroot image/casper/filesystem.squashfs \
-        -noappend -no-duplicates -no-recovery \
-        -wildcards \
-        -e "var/cache/apt/archives/*" \
-        -e "root/*" \
-        -e "root/.*" \
-        -e "tmp/*" \
-        -e "tmp/.*" \
-        -e "swapfile"
-    printf $(sudo du -sx --block-size=1 chroot | cut -f1) > image/casper/filesystem.size
+    # we need to install systemd first, to configure machine id
+    apt-get update
+    apt-get install -y libterm-readline-gnu-perl systemd-sysv
 
-    # create diskdefines
-    cat <<EOF > image/README.diskdefines
-#define DISKNAME  ${GRUB_LIVEBOOT_LABEL}
-#define TYPE  binary
-#define TYPEbinary  1
-#define ARCH  amd64
-#define ARCHamd64  1
-#define DISKNUM  1
-#define DISKNUM1  1
-#define TOTALNUM  0
-#define TOTALNUM0  1
+    #configure machine id
+    dbus-uuidgen > /etc/machine-id
+    ln -fs /etc/machine-id /var/lib/dbus/machine-id
+
+    # don't understand why, but multiple sources indicate this
+    dpkg-divert --local --rename --add /sbin/initctl
+    ln -s /bin/true /sbin/initctl
+}
+
+# Load configuration values from file
+function load_config() {
+    if [[ -f "$SCRIPT_DIR/config.sh" ]]; then 
+        . "$SCRIPT_DIR/config.sh"
+    elif [[ -f "$SCRIPT_DIR/default_config.sh" ]]; then
+        . "$SCRIPT_DIR/default_config.sh"
+    else
+        >&2 echo "Unable to find default config file  $SCRIPT_DIR/default_config.sh, aborting."
+        exit 1
+    fi
+}
+
+
+function install_pkg() {
+    echo "=====> running install_pkg ... will take a long time ..."
+    apt-get -y upgrade
+
+    # install live packages
+    apt-get install -y \
+    sudo \
+    ubuntu-standard \
+    casper \
+    discover \
+    laptop-detect \
+    os-prober \
+    network-manager \
+    resolvconf \
+    net-tools \
+    wireless-tools \
+    wpagui \
+    grub-common \
+    grub-gfxpayload-lists \
+    grub-pc \
+    grub-pc-bin \
+    grub2-common \
+    locales
+    
+    # install kernel
+    apt-get install -y --no-install-recommends $TARGET_KERNEL_PACKAGE
+
+    # graphic installer - ubiquity
+    apt-get install -y \
+    ubiquity \
+    ubiquity-casper \
+    ubiquity-frontend-gtk \
+    ubiquity-slideshow-ubuntu \
+    ubiquity-ubuntu-artwork
+
+    # Call into config function
+    customize_image
+
+    # remove unused and clean up apt cache
+    apt-get autoremove -y
+
+    # final touch
+    dpkg-reconfigure locales
+    dpkg-reconfigure systemd-resolved
+
+    # network manager
+    cat <<EOF > /etc/NetworkManager/NetworkManager.conf
+[main]
+plugins=ifupdown,keyfile
+dns=dnsmasq
+
+[ifupdown]
+managed=false
 EOF
 
-    # create iso image
-    pushd $DIR/image
-    grub-mkstandalone \
-        --format=x86_64-efi \
-        --output=isolinux/bootx64.efi \
-        --locales="" \
-        --fonts="" \
-        "boot/grub/grub.cfg=isolinux/grub.cfg"
+    dpkg-reconfigure network-manager
 
-    (
-        cd isolinux && \
-        dd if=/dev/zero of=efiboot.img bs=1M count=10 && \
-        sudo mkfs.vfat efiboot.img && \
-        LC_CTYPE=C mmd -i efiboot.img efi efi/boot && \
-        LC_CTYPE=C mcopy -i efiboot.img ./bootx64.efi ::efi/boot/
-    )
+    apt-get clean -y
+}
 
-    grub-mkstandalone \
-        --format=i386-pc \
-        --output=isolinux/core.img \
-        --install-modules="linux16 linux normal iso9660 biosdisk memdisk search tar ls" \
-        --modules="linux16 linux normal iso9660 biosdisk search" \
-        --locales="" \
-        --fonts="" \
-        "boot/grub/grub.cfg=isolinux/grub.cfg"
+function finish_up() { 
+    echo "=====> finish_up"
 
-    cat /usr/lib/grub/i386-pc/cdboot.img isolinux/core.img > isolinux/bios.img
+    # truncate machine id (why??)
+    truncate -s 0 /etc/machine-id
 
-    sudo /bin/bash -c "(find . -type f -print0 | xargs -0 md5sum | grep -v -e 'md5sum.txt' -e 'bios.img' -e 'efiboot.img' > md5sum.txt)"
+    # remove diversion (why??)
+    rm /sbin/initctl
+    dpkg-divert --rename --remove /sbin/initctl
 
-    sudo xorriso \
-        -as mkisofs \
-        -iso-level 3 \
-        -full-iso9660-filenames \
-        -volid "$TARGET_NAME" \
-        -eltorito-boot boot/grub/bios.img \
-        -no-emul-boot \
-        -boot-load-size 4 \
-        -boot-info-table \
-        --eltorito-catalog boot/grub/boot.cat \
-        --grub2-boot-info \
-        --grub2-mbr /usr/lib/grub/i386-pc/boot_hybrid.img \
-        -eltorito-alt-boot \
-        -e EFI/efiboot.img \
-        -no-emul-boot \
-        -append_partition 2 0xef isolinux/efiboot.img \
-        -output "$DIR/$TARGET_NAME.iso" \
-        -m "isolinux/efiboot.img" \
-        -m "isolinux/bios.img" \
-        -graft-points \
-           "/EFI/efiboot.img=isolinux/efiboot.img" \
-           "/boot/grub/bios.img=isolinux/bios.img" \
-           "."
-
-    popd
+    rm -rf /tmp/* ~/.bash_history
 }
 
 # =============   main  ================
 
-# we always stay in $DIR
-cd $DIR
-
 load_config
-check_config
 check_host
 
 # check number of args
@@ -311,7 +197,7 @@ if [[ $dash_flag == false ]]; then
     end_index=$(($start_index + 1))
 fi
 
-#loop through the commands
+# loop through the commands
 for ((ii=$start_index; ii<$end_index; ii++)); do
     ${COMMANDS[ii]}
 done
