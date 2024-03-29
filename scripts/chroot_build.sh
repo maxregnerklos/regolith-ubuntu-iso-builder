904d35e1f539464cc2782e922c72db21e5a57b1e
maxregnerklos <135330858+maxregnerklos@users.noreply.github.com>
2024-03-29 06:00:22 -0400

    Update chroot_build.sh

--- scripts/chroot_build.sh
+++ scripts/chroot_build.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
-# Rephrased code for building Ubuntu from scratch
+# Concept OS build script based on Ubuntu 25.04
 
 set -e                  # exit on error
 set -o pipefail         # exit on pipeline error
 set -u                  # treat unset variable as error
-#set -x
 
+# Set script directory
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-COMMANDS=(setup_host install_pkg finish_up)
+# Define commands
+COMMANDS=(setup_host install_pkg customize_image finish_up)
 
+# Help function
 function help() {
-    # if $1 is set, use $1 as headline message in help()
-    if [ -z ${1+x} ]; then
-        echo -e "This script builds Ubuntu from scratch"
-        echo -e
-    else
-        echo -e $1
-        echo
-    fi
-    echo -e "Supported commands : ${COMMANDS[*]}"
+    echo -e "This script builds a concept OS based on Ubuntu 25.04"
+    echo -e
+    echo -e "Supported commands: ${COMMANDS[*]}"
     echo -e
     echo -e "Syntax: $0 [start_cmd] [-] [end_cmd]"
-    echo -e "\trun from start_cmd to end_end"
-    echo -e "\tif start_cmd is omitted, start from first command"
-    echo -e "\tif end_cmd is omitted, end with last command"
-    echo -e "\tenter single cmd to run the specific command"
-    echo -e "\tenter '-' as only argument to run all commands"
+    echo -e "\trun from start_cmd to end_cmd"
+    echo -e "\tif start_cmd is omitted, start from the first command"
+    echo -e "\tif end_cmd is omitted, end with the last command"
+    echo -e "\tenter a single cmd to run the specific command"
+    echo -e "\tenter '-' as the only argument to run all commands"
     echo -e
     exit 0
 }
 
+# Function to find index of a command
 function find_index() {
     local ret;
     local i;
@@ -43,6 +40,7 @@ function find_index() {
     help "Command not found : $1"
 }
 
+# Function to check if the script is run as root
 function check_host() {
     if [ $(id -u) -ne 0 ]; then
         echo "This script should be run as 'root'"
@@ -53,53 +51,39 @@ function check_host() {
     export LC_ALL=C
 }
 
+# Function to set up the host environment
 function setup_host() {
     echo "=====> running setup_host ..."
 
-   cat <<EOF > /etc/apt/sources.list
-deb $TARGET_UBUNTU_MIRROR $TARGET_UBUNTU_VERSION main restricted universe multiverse
-deb-src $TARGET_UBUNTU_MIRROR $TARGET_UBUNTU_VERSION main restricted universe multiverse
+    cat <<EOF > /etc/apt/sources.list
+deb http://archive.ubuntu.com/ubuntu/ focal main restricted universe multiverse
+deb-src http://archive.ubuntu.com/ubuntu/ focal main restricted universe multiverse
 
-deb $TARGET_UBUNTU_MIRROR $TARGET_UBUNTU_VERSION-security main restricted universe multiverse
-deb-src $TARGET_UBUNTU_MIRROR $TARGET_UBUNTU_VERSION-security main restricted universe multiverse
+deb http://archive.ubuntu.com/ubuntu/ focal-security main restricted universe multiverse
+deb-src http://archive.ubuntu.com/ubuntu/ focal-security main restricted universe multiverse
 
-deb $TARGET_UBUNTU_MIRROR $TARGET_UBUNTU_VERSION-updates main restricted universe multiverse
-deb-src $TARGET_UBUNTU_MIRROR $TARGET_UBUNTU_VERSION-updates main restricted universe multiverse
+deb http://archive.ubuntu.com/ubuntu/ focal-updates main restricted universe multiverse
+deb-src http://archive.ubuntu.com/ubuntu/ focal-updates main restricted universe multiverse
 EOF
 
-    echo "$TARGET_NAME" > /etc/hostname
+    echo "ConceptOS" > /etc/hostname
 
-    # we need to install systemd first, to configure machine id
     apt-get update
-    apt-get install -y libterm-readline-gnu-perl systemd-sysv
+    apt-get install -y systemd-sysv dbus
 
-    #configure machine id
     dbus-uuidgen > /etc/machine-id
     ln -fs /etc/machine-id /var/lib/dbus/machine-id
 
-    # don't understand why, but multiple sources indicate this
     dpkg-divert --local --rename --add /sbin/initctl
     ln -s /bin/true /sbin/initctl
 }
 
-# Load configuration values from file
-function load_config() {
-    if [[ -f "$SCRIPT_DIR/config.sh" ]]; then 
-        . "$SCRIPT_DIR/config.sh"
-    elif [[ -f "$SCRIPT_DIR/default_config.sh" ]]; then
-        . "$SCRIPT_DIR/default_config.sh"
-    else
-        >&2 echo "Unable to find default config file  $SCRIPT_DIR/default_config.sh, aborting."
-        exit 1
-    fi
-}
-
-
+# Function to install necessary packages
 function install_pkg() {
-    echo "=====> running install_pkg ... will take a long time ..."
+    echo "=====> running install_pkg ..."
+
     apt-get -y upgrade
 
-    # install live packages
     apt-get install -y \
     sudo \
     ubuntu-standard \
@@ -118,11 +102,9 @@ function install_pkg() {
     grub-pc-bin \
     grub2-common \
     locales
-    
-    # install kernel
-    apt-get install -y --no-install-recommends $TARGET_KERNEL_PACKAGE
 
-    # graphic installer - ubiquity
+    apt-get install -y --no-install-recommends linux-image-generic-hwe-20.04
+
     apt-get install -y \
     ubiquity \
     ubiquity-casper \
@@ -130,17 +112,9 @@ function install_pkg() {
     ubiquity-slideshow-ubuntu \
     ubiquity-ubuntu-artwork
 
-    # Call into config function
-    customize_image
-
-    # remove unused and clean up apt cache
-    apt-get autoremove -y
-
-    # final touch
     dpkg-reconfigure locales
     dpkg-reconfigure systemd-resolved
 
-    # network manager
     cat <<EOF > /etc/NetworkManager/NetworkManager.conf
 [main]
 plugins=ifupdown,keyfile
@@ -152,36 +126,53 @@ EOF
 
     dpkg-reconfigure network-manager
 
+    apt-get autoremove -y
     apt-get clean -y
 }
 
-function finish_up() { 
+# Function to customize the image
+function customize_image() {
+    echo "=====> running customize_image ..."
+
+    # Customization steps go here
+}
+
+# Function to perform finishing tasks
+function finish_up() {
     echo "=====> finish_up"
 
-    # truncate machine id (why??)
     truncate -s 0 /etc/machine-id
 
-    # remove diversion (why??)
     rm /sbin/initctl
     dpkg-divert --rename --remove /sbin/initctl
 
     rm -rf /tmp/* ~/.bash_history
 }
 
-# =============   main  ================
+# Load configuration values
+function load_config() {
+    if [[ -f "$SCRIPT_DIR/config.sh" ]]; then 
+        . "$SCRIPT_DIR/config.sh"
+    elif [[ -f "$SCRIPT_DIR/default_config.sh" ]]; then
+        . "$SCRIPT_DIR/default_config.sh"
+    else
+        >&2 echo "Unable to find default config file  $SCRIPT_DIR/default_config.sh, aborting."
+        exit 1
+    fi
+}
 
+# Main execution starts here
 load_config
 check_host
 
-# check number of args
-if [[ $# == 0 || $# > 3 ]]; then help; fi
+if [[ $# == 0 || $# > 3 ]]; then
+    help
+fi
 
-# loop through args
 dash_flag=false
 start_index=0
 end_index=${#COMMANDS[*]}
-for ii in "$@";
-do
+for ii in "$@"; do
     if [[ $ii == "-" ]]; then
         dash_flag=true
         continue
@@ -193,11 +184,11 @@ do
         end_index=$(($index+1))
     fi
 done
+
 if [[ $dash_flag == false ]]; then
     end_index=$(($start_index + 1))
 fi
 
-# loop through the commands
 for ((ii=$start_index; ii<$end_index; ii++)); do
     ${COMMANDS[ii]}
 done
